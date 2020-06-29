5b5f92f4512921c54bfbbe226174ca3927285c3f
evansinho <igiri.evanson@gmail.com>
2020-06-29 15:43:23 +0100

    added session controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root "welcome#index"
   get "/about", to: "welcome#about"
   get '/signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :articles
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
